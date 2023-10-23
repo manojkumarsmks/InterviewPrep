6fb19dadcb06bf1e1f63a9a80cc7655a59f4a827
manojkumarsmks <manojkumarsmks@gmail.com>
2023-10-22 18:25:30 -0600

    solution

---
+++ src/Arrays/Problem1929.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+import java.util.Arrays;
+
+/* https://leetcode.com/problems/concatenation-of-array/description/ */
+
+public class Problem1929 {
+    public static void main(String[] args) {
+        int[] nums = { 1, 3, 2, 1 };
+        System.out.println(Arrays.toString(getConcatenation(nums)));
+    }
+
+    public static int[] getConcatenation(int[] nums) {
+        int[] ans = new int[nums.length * 2];
+
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[i];
+            ans[i + nums.length] = nums[i];
+
+        }
+
+        System.gc();
+
+        return ans;
+    }
+}
